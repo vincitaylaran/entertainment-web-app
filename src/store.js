6c314d69e29f32261cf410f69a104bfec3716ac7
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-04-14 12:30:31 -0600

    Create store using dev tools

--- src/store.js
+++ src/store.js
@@ -1,7 +1,25 @@
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 import catalogue from './data.json'
+import { enhancedDispatch, enhancedGetState } from './addons/enhancers'
+import { print1, print2, print3 } from './addons/middleware'
 
-const store = createStore(rootReducer, { bookmarks: [], catalogue })
+const preloadedState = {
+  // Sets initial state for bookmarks to be an empty array.
+  bookmarks: [],
+
+  // Sets initial state for catalogue to be the sample data provided
+  // by FE Mentor.
+  catalogue,
+}
+
+// Merges all of our enhancers into 1 mega-enhancer.
+// const composedEnhancer = compose(enhancedDispatch, enhancedGetState)
+
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+const composedEnhancer = composeWithDevTools()
+
+const store = createStore(rootReducer, preloadedState, composedEnhancer)
 
 export default store
