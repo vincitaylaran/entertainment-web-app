72795c95ac4f5bfb8db6b8639f7f4d0351244ebf
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-04-15 15:59:30 -0600

    Update styles to CatalogueItem if item is trending

--- src/common/components/CatalogueItem.js
+++ src/common/components/CatalogueItem.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Box from './Box'
 import Bookmark from '../../features/bookmark/Bookmark'
 import './CatalogueItem.css'
+import { ReactComponent as MovieIcon } from '../../svgs/icon-category-movie.svg'
+import { ReactComponent as TVIcon } from '../../svgs/icon-category-tv.svg'
 
 /**
  * ITEM SCHEMA
@@ -34,13 +36,13 @@ function CatalogueItem({ item }) {
 }
 
 function CatalogueItemImage(props) {
-  const { thumbnail, title } = props
+  const { thumbnail, title, isTrending } = props
 
   return (
     <div className='CatalogueItemImage-container'>
       <img
         className='CatalogueItemImage-main'
-        src={thumbnail.regular.large}
+        src={isTrending ? thumbnail.trending.large : thumbnail.regular.large}
         alt={title}
       />
       <Bookmark className='CatalogueItemImage-bookmark' item={props} />
@@ -48,12 +50,21 @@ function CatalogueItemImage(props) {
   )
 }
 
-function CatalogueItemInfo({ title, year, category, rating }) {
+function CatalogueItemInfo({ title, year, category, rating, isTrending }) {
   return (
-    <div className='CatalogueItemInfo-container'>
+    <div
+      className={`CatalogueItemInfo-container ${isTrending ? 'trending' : ''}`}
+    >
       <div className='CatalogueItemInfo-sub'>
-        <span className='CatalogueItemInfo-year'>{year}</span>{' '}
-        <span className='CatalogueItemInfo-category'>{category}</span>{' '}
+        <span className='CatalogueItemInfo-year'>{year}</span>
+        <span className='bullet-point'>•</span>
+        <span className='CatalogueItemInfo-category'>
+          <span className='CatalogueItemInfo-category-icon'>
+            {category === 'Movie' ? <MovieIcon /> : <TVIcon />}
+          </span>
+          {category}
+        </span>
+        <span className='bullet-point'>•</span>
         <span className='CatalogueItemInfo-rating'>{rating}</span>
       </div>
       <h2 className='CatalogueItemInfo-title'>{title}</h2>
