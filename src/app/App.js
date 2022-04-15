06e2d79ffd790ebacc0acdfee7dd23b7624695b1
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-04-15 15:04:38 -0600

    Move /assets to /public, create /svgs inside /src

--- src/app/App.js
+++ src/app/App.js
@@ -1,3 +1,6 @@
+import { useSelector } from 'react-redux'
+import { v4 as uuidv4 } from 'uuid'
+
 import '../App.css'
 
 import Input from '../common/components/Input'
@@ -5,14 +8,24 @@ import SearchInput from '../common/components/SearchInput'
 import Button from '../common/components/Button'
 
 import Bookmark from '../features/bookmark/Bookmark'
+import Box from '../common/components/Box'
+import CatalogueItem from '../common/components/CatalogueItem'
+
+const selectCatalogue = (state) => state.catalogue
 
 function App() {
+  const catalogue = useSelector(selectCatalogue)
+
   return (
     <div className='App'>
       <Input placeholder='Email address' />
       <SearchInput />
       <Button text='Login to your account' />
       <Bookmark />
+      <Box>Hello</Box>
+      {catalogue.map((item) => (
+        <CatalogueItem item={item} key={uuidv4()} />
+      ))}
     </div>
   )
 }
