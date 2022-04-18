62e8b58fe6e92b5d19a5c0c83be64095e5e6ef12
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-04-18 11:34:46 -0600

    Update store by removing preloadedState

--- src/app/store.js
+++ src/app/store.js
@@ -1,25 +1,13 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
-import catalogue from '../data.json'
-import { enhancedDispatch, enhancedGetState } from '../addons/enhancers'
 import { print1, print2, print3 } from '../addons/middleware'
 
-const preloadedState = {
-  // Sets initial state for bookmarks to be an empty array.
-  bookmarks: [],
-
-  // Sets initial state for catalogue to be the sample data provided
-  // by FE Mentor.
-  catalogue,
-}
-
-// Merges all of our enhancers into 1 mega-enhancer.
-// const composedEnhancer = compose(enhancedDispatch, enhancedGetState)
-
 const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+
+// Lets our app use the browser extension, Redux DevTools.
 const composedEnhancer = composeWithDevTools()
 
-const store = createStore(rootReducer, preloadedState, composedEnhancer)
+const store = createStore(rootReducer, composedEnhancer)
 
 export default store
