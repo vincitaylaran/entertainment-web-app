81c23c52f15038534c21eb615e579301d1a63b4e
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2022-04-18 15:19:16 -0600

    Create CatalogueItems component

---
+++ src/common/components/CatalogueItems.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './CatalogueItems.css'
+import CatalogueItem from './CatalogueItem'
+import { v4 as uuid } from 'uuid'
+
+const generateCatalogue = (catalogue, isTrending) => {
+  if (isTrending) {
+    return catalogue.filter((item) => item.isTrending)
+  }
+
+  return catalogue.filter((item) => !item.isTrending)
+}
+
+/**
+ *
+ * @param {*} props: { catalogue: array, isTrending: boolean }
+ * @returns
+ */
+function CatalogueItems({ catalogue, isTrending }) {
+  const filteredCatalogue = generateCatalogue(catalogue, isTrending)
+
+  return (
+    <div className='CatalogueItems'>
+      <h1 className='CatalogueItems-section-name'>Recommended for you</h1>
+      <div className={`CatalogueItems-grid${isTrending ? '--trending' : ''}`}>
+        {filteredCatalogue.map((item) => (
+          <CatalogueItem item={item} key={uuid()} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CatalogueItems
